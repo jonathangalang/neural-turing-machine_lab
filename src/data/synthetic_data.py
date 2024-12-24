46249d4412491cf4488480897f72c9b8e0e329a5
Nate <jonathan.galang@utexas.edu>
2024-12-24 04:05:09 -0600

    Outlined copy problem

---
+++ src/data/synthetic_data.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def generate_copy_problem(signal_length, delay, batch_size):
+    """
+
+    Args:
+        signal_length (int): the number of 8-bit integers to generate
+        delay (int): the number of time-steps to delay before the recall signal
+        batch_size (int): the number of copy problems to generate
+    """
+    pass
