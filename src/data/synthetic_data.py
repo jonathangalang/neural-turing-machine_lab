42885bc8b7a74713546602d207faaacb6d1d4c90
Nate <jonathan.galang@utexas.edu>
2024-12-30 03:16:01 -0600

    added statement to generate pseudorandom sequences of 8-bit integers

--- src/data/synthetic_data.py
+++ src/data/synthetic_data.py
@@ -1,4 +1,6 @@
-import numpy as np
+import torch
+
+INT_MAX = (2 ** 8) + 1
 
 def generate_copy_problem(signal_length, delay, batch_size):
     """
@@ -8,4 +10,7 @@ def generate_copy_problem(signal_length, delay, batch_size):
         delay (int): the number of time-steps to delay before the recall signal
         batch_size (int): the number of copy problems to generate
     """
-    pass
+    # generate batches of sequences of random 8-bit integers
+    signal = torch.randint(0, INT_MAX, (batch_size, signal_length), dtype=torch.uint8) 
+    
+
