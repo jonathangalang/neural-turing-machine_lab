84ef2a8da9b395a817d3eba304e4ee7ea3ea2238
Nate <jonathan.galang@utexas.edu>
2024-12-30 22:03:16 -0600

    Added logic for loading config from yaml file

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,9 +1,10 @@
 import src.utils.logs as logs
 import torch
+import yaml
 
 logger = logs.get_logger(__name__)
 
-def setup():
+def enable_logs():
     logs.configure()
     torch.set_default_device('cuda')
     if 'cuda' not in torch.get_default_device().type:
@@ -11,3 +12,7 @@ def setup():
     else:
         logger.info('Successfully set default device to CUDA.')
 
+def load_config(config_path):
+    with open(config_path, 'r') as config_file:
+        config = yaml.safe_load(config_file)
+    print(config)
