236a00c5d4db7d741a29870e535ac80319076732
Nate <jonathan.galang@utexas.edu>
2024-12-30 22:03:30 -0600

    Added default behavior for sanity check

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
-from src.utils.config import setup
+import argparse
+import os
+import sys
+
+from src.utils import config
+from src.data.synthetic_data import sanity_check
+
+def parse_args():
+    """
+
+    Returns:
+        argparse.Namespace: parsed arguments
+        
+    """
+    parser = argparse.ArgumentParser("NTM Lab")
+    parser.add_argument("run", help="Run a training experiment")
+    parser.add_argument("--config", type=str,
+                        default="experiments/configs/default_config.yaml",
+                        help="Path to the YAML config file.")
+    return parser.parse_args()
 
 if __name__ == '__main__':
-    setup()
+    argv = parse_args()
+    
+    config.enable_logs()
+
+    config.load_config(argv.config)
+    
+    sanity_check()
+
